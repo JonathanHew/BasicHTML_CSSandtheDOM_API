9fe51204a8318ae764b6f09f7da92606d02c30b4
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-10-09 16:15:11 +0100

    createNotes function implemented and working

--- problem-2/Assets/Scripts/site.js
+++ problem-2/Assets/Scripts/site.js
@@ -1,6 +1,10 @@
 let form = document.getElementById("form");
 let titleInput = document.getElementById("titleInput");
 let msg = document.getElementById("msg");
+let bodyInput = document.getElementById("bodyInput");
+let colorInput = document.getElementById("colorInput");
+let notes = document.getElementById("notes");
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -17,11 +21,30 @@ let formValidation = () =>{
     {
         console.log("Success!!");
         msg.innerHTML = "";
+        acceptData();
     }
 };
 
 let data = {};
 
 let acceptData = () => {
-    
+    data["title"] = titleInput.value;
+    data["body"] = bodyInput.value;
+    data["color"] = colorInput.value;
+
+    console.log(data)
+    createNotes();
+};
+
+let createNotes = () => {
+    notes.innerHTML += `
+    <div>
+        <span class="fw-bold">${data.title}</span>
+        <p>${data.body}</p>
+        <span class="options">
+        <button name="edit" type="button">Edit</button>
+        <button name="delete" type="button">Delete</button> 
+        </span>
+    </div>
+    `;
 };
